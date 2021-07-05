26a073fa047d7a5abe2525e995111676e726feec
Afrizzal P. Pratama <apratama8@gmail.com>
2021-07-05 20:32:01 +0700

    first init

--- introduce/main.swift
+++ introduce/main.swift
@@ -6,6 +6,18 @@
 //
 
 import Foundation
+// Start form.
+print("Welcome to my repo")
 
-print("Hello, World!")
+let firstName = "John", lastName = "Doe"
+let fullName = firstName + " " + lastName
+let address = "Memphis"
+let job = "iOS Developer"
+let age = 0b10001
 
+print("----------------------------------")
+// Ini adalah output dari program yang Anda buat.
+print("Did you know \(fullName)?")
+print("\(firstName) is a \(job).")
+print("is a \(age) y.o and livin in \(address).")
+print("----------------------------------")
